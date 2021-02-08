a0ece6839757bc4a53fd6c465f92f2d7e35e8ff9
semihyonet <semihyonet@gmail.com>
2021-02-08 17:01:34 +0300

    Added Like models and Like Routes

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const sequelize = require("sequelize");
 
 const Comment = require("../models/comment");
 const Episode = require("../models/episode");
 const Content = require("../models/content");
+const Like = require("../models/like");
 
 router.get("/comment/:id", (req, res) => {
 	Comment.findByPk(req.params.id).then((comment) => {
@@ -29,10 +31,25 @@ router.get("/comment/episode/:id", async (req, res) => {
 	const episode = await Episode.findByPk(req.params.id);
 
 	const comments = await episode.getComments();
-	res.send(comments);
-	// Comment.create({ userId, contentId, isMovie, text }).then((response) => {
-	// 	res.send(response);
-	// });
+
+	const result = [];
+	for await (const comment of comments) {
+		const likes = await Like.findAll({
+			attributes: [
+				[sequelize.fn("COUNT", sequelize.col("like.userId")), "likes"],
+			],
+			where: {
+				commentId: comment.id,
+			},
+		});
+		console.log(likes[0].dataValues.likes);
+		result.push({
+			comment: comment.dataValues,
+			like: likes[0].dataValues.likes,
+		});
+	}
+
+	res.send(result);
 });
 
 router.post("/comment/content/:id", async (req, res) => {
@@ -53,7 +70,24 @@ router.get("/comment/content/:id", async (req, res) => {
 	const content = await Content.findByPk(req.params.id);
 
 	const comments = await content.getComments();
-	res.send(comments);
+	const result = [];
+	for await (const comment of comments) {
+		const likes = await Like.findAll({
+			attributes: [
+				[sequelize.fn("COUNT", sequelize.col("like.userId")), "likes"],
+			],
+			where: {
+				commentId: comment.id,
+			},
+		});
+		console.log(likes[0].dataValues.likes);
+		result.push({
+			comment: comment.dataValues,
+			like: likes[0].dataValues.likes,
+		});
+	}
+
+	res.send(result);
 	// Comment.create({ userId, contentId, isMovie, text }).then((response) => {
 	// 	res.send(response);
 	// });
